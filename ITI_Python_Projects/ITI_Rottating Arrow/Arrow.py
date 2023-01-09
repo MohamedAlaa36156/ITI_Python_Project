51ee12f70d820b9703674c3e04d47c661f6773eb
Mohamed Alaa <116747353+MohamedAlaa36156@users.noreply.github.com>
2023-01-10 01:00:39 +0200

    Add files via upload

--- ITI_Python_Projects/ITI_Rottating Arrow/Arrow.py
+++ ITI_Python_Projects/ITI_Rottating Arrow/Arrow.py
@@ -3,7 +3,7 @@ import time
 n=int(input("Enter the height of the arrows"))
 while(1):
 	
-	for i in range(n):
+	for i in range(n):#arrow Pointing Toward's UPWARDS
 		for j in range(i,n):
 			print(" ",end=" ")
 		for j in range(i):
@@ -11,22 +11,33 @@ while(1):
 		for j in range(i+1):
 			print("*",end=" ")
 		print() 
+	for i in range(n):
+		print(n*"  ",end="*\n")
+	
+
+		
 	time.sleep(1)
 	os.system("cls")
-	for i in range(n):
+	for i in range(n):	#arrow Pointing Toward's LEFT
 		for j in range (n-i):
 			print(" ",end=" ")
 		for j in range(i):
 			print("*",end=" ")
 		print()
 	for i in range(n):
-		for j in range (i):
-			print(" ",end=" ")
-		for j in range(n-i):
-			print("*",end=" ")
+		if(i==0):
+			print(2*(n)*"* ",end="")
+		if(i!=0):
+			for j in range (i):
+				print(" ",end=" ")
+			for j in range(n-i):
+				print("*",end=" ")
 		print()
 	time.sleep(1)
 	os.system("cls")
+	
+	for i in range(n):#arrow Pointing Toward's Downwards
+		print(n*"  ",end="*\n")
 	
 	for i in range(n):
 		for j in range(i+1):
@@ -40,20 +51,31 @@ while(1):
 	time.sleep(1)
 	os.system("cls")		
 		
-	for i in range(n):
-		for j in range (i):
-			print("*",end=" ")
-		for j in range(n-i):
-			print(" ",end=" ")
-		print()
-	for i in range(n):
-		for j in range (n-i):
-			print("*",end=" ")
-		for j in range(n-i):
-			print(" ",end=" ")
-		print()
+	# for i in range(n):
+		# for j in range (i):
+			# print("*",end=" ")
+		# for j in range(n-i):
+			# print(" ",end=" ")
+		# print()
+	# for i in range(n):
+		# for j in range (n-i):
+			# print("*",end=" ")
+		# for j in range(n-i):
+			# print(" ",end=" ")
+		# print()
+	
+	for i in range(n):								#arrow Pointing Toward's Right
+		if i== n-1: 
+			print((2*n)*"*", end=" ")
+			print((i+1)* "*")
+		else:
+			print((2*n)* " ", end =" ")
+			print((i+1)* "*")
+	for j in range(n-1,0,-1):
+		print((2*n)* " ", end= " ")
+		print(j* "*")
+
+
+
 	time.sleep(1)
-	
-		
-	
 	os.system("cls")
